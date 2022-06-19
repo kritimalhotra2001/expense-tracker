03909f1eae055810e9ca22dbdec867d60cf2ceef
Mohinder Garg <vmsbuilders@Mohinders-Air.Dlink>
2022-06-19 23:04:52 +0530

    UI Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Header from "./compoments/Header";
-import Budget from "./compoments/Budget";
-import Remaining from "./compoments/Remaining";
-import ExpenseTotal from "./compoments/ExpenseTotal";
-import ExpenseList from "./compoments/ExpenseList";
-import AddExpenseForm from "./compoments/AddExpenseForm";
+import Header from "./components/Header";
+import Budget from "./components/Budget";
+import Remaining from "./components/Remaining";
+import ExpenseTotal from "./components/ExpenseTotal";
+import ExpenseList from "./components/ExpenseList";
+import AddExpenseForm from "./components/AddExpenseForm";
 
 const App = () => {
   return (
